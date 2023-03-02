1d7b4730b0a7aa76166411c4d5edf31fa26ef092
Kasdein <kasdein@outlook.fr>
2023-03-02 14:31:24 +0000

    ar redone (interaction needs work)

---
+++ Assets/Scripts/SelectedState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedState : MonoBehaviour
+{
+    public Material materialSelected;
+    private Material materialDefault;
+    public AudioClip audioClip;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+        audioSource.clip = audioClip;
+        materialDefault = GetComponent<MeshRenderer>().material;
+    }
+
+    public void playAudio()
+    {
+        audioSource.Play();
+    }
+
+    public void setSelectedMaterial ()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = materialSelected;
+    }
+
+    public void setDefaultMaterial ()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = materialDefault;
+    }
+
+
+}
