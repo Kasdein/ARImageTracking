4d749b59ffd3916ce07d74040a0c21ba3a18d1f5
Kasdein <kasdein@outlook.fr>
2023-03-16 14:39:55 +0000

    fixed alpha on anim + anim script

--- Assets/Scripts/Animation.cs
+++ Assets/Scripts/Animation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,40 @@ using UnityEngine;
 public class Animation : MonoBehaviour
 {
     public List<Texture> frames = new List<Texture>();
-    private Texture firstFrame;
     private int frameCount;
+    public Material myMaterial;
+    public float lengthOfFrame = 0.0417f;
+    public float timeSinceLastFrame;
+
     // Start is called before the first frame update
     void Start()
     {
-        firstFrame = gameObject.GetComponent<MeshRenderer>().material.GetTexture("Walk cycle000");
-        frameCount = 0;
+
     }
 
-    public IEnumerator animationDelay()
-    {
-
-        yield return null;
-    }
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceLastFrame += Time.deltaTime;
+
+        if (timeSinceLastFrame >= lengthOfFrame)
+        {
+            NextFrame();
+            timeSinceLastFrame = 0;
+        }
+    }
+
+    void NextFrame()
+    {
+        if (frameCount < frames.Count - 1)
+        {
+            frameCount++;
+        }
+        else
+        {
+            frameCount = 0;
+        }
+
+        myMaterial.SetTexture("_MainTex", frames[frameCount]);
     }
 }
